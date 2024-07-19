9649016b835e7f57021ee0763763fa47c3df700d
srivarenya12 <110167217+srivarenya12@users.noreply.github.com>
2024-07-19 14:35:08 +0530

    Update Avalanche.sol

--- Avalanche.sol
+++ Avalanche.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "varenya";
-    string public symbol = "VAR";
+    string public name = "wow";
+    string public symbol = "TYU";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
